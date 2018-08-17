7b4aadfcde884d8fcd1567aa01a749968484a3cb
John Grubbs <johnmlowery@gmail.com>
2018-08-17 23:36:51 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -3,47 +3,45 @@ def welcome
 end
 
 def deal_card
-  1 + rand(11)
+  card = rand(1..11) #reuse the randomize method by setting it equal to a variable
 end
 
-def display_card_total(deal_card)
-  deal_card = deal_card
-  puts "Your cards add up to #{deal_card}"
+def display_card_total(card_total)
+  puts "Your cards add up to #{card_total}"
 end
 
 def prompt_user
-    puts "Type 'h' to hit or 's' to stay"
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-	  awnser = gets.chomp
-	 awnser == "s" || awnser == "h"
-	 return awnser
-  end
+  user_input = gets.chomp
+end
 
-def end_game(sorry)
-  puts "Sorry, you hit 27. Thanks for playing!"
+def end_game(card_total)
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
-  n = deal_card + deal_card
-  puts "Your cards add up to #{n}"
-  return n
+  card_one = deal_card
+  card_two = deal_card
+  card_total = card_one + card_two
+  display_card_total(card_total) #once you have figured out the sum of your two cards, you can pass the total in as an argumnt so your phrase is displayed.
+  return card_total
 end
 
 def hit?(card_total)
-  prompt_user
-  user_input = get_user_input 
+  prompt_user #ask the user if they want to hit or stay
+  user_input = get_user_input #set a variable to use for your if/else statement
   if user_input == "h"
-    card_total += deal_card 
+    card_total += deal_card #increase your former total by the new number given by dealing another card
   elsif user_input == "s"
-    card_total 
+    card_total #return the number and prompt user to hit again
   else
     invalid_command
   end
 end
-  
-  
+
 def invalid_command
   puts "Please enter a valid command"
   prompt_user
@@ -63,4 +61,5 @@ def runner
   end
   end_game(sum)
 end
-    
+
+runner 
